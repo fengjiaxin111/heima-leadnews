4786600c495a3488f88b9dddecec6465ff01e696
feng <you@example.com>
2023-09-25 22:12:42 +0800

    Day2-08

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/mapper/ApArticleMapper.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/mapper/ApArticleMapper.java
@@ -1,28 +1,16 @@
 package com.heima.article.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.heima.model.user.dtos.ArticleHomeDto;
+import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-/**
- * @author 冯佳鑫
- * @Package：com.heima.article.mapper
- * @Project：heima-leadnews
- * @name：ApArticleMapper
- * @Date：2023/9/24 15:05
- * @Filename：ApArticleMapper
- */
 @Mapper
 public interface ApArticleMapper extends BaseMapper<ApArticle> {
-    /**
-     *type
-     * @param dto
-     * @param type 1加载更多 2加载最新
-     * @return
-     */
+
     public List<ApArticle> loadArticleList(@Param("dto") ArticleHomeDto dto, @Param("type") Short type);
+
 }
